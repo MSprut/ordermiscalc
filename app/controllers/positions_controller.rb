3cdb66bfbe754ab3a3e9ce6f65872ab4406d8b7d
Evgeniy Martynenko <msprut2000@gmail.com>
2018-11-26 17:17:27 +0300

    fixed position sort and style homepage

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,31 +1,23 @@
 class PositionsController < ApplicationController
   before_action :set_position, only: [:show, :edit, :update, :destroy]
 
-  # GET /positions
-  # GET /positions.json
   def index
-    @positions = Position.where(deleted: false)
+    @positions = Position.where(deleted: false).order(name: :asc)
   end
 
-  # GET /positions/1
-  # GET /positions/1.json
   def show
   end
 
-  # GET /positions/new
   def new
     @position = Position.new
     @position.position_salaries.build
   end
 
-  # GET /positions/1/edit
   def edit
     @position = Position.find(params[:id])
     @position_salaries = PositionSalary.get_last(@position.id, 5)
   end
 
-  # POST /positions
-  # POST /positions.json
   def create
     @position = Position.new(position_params)
 
@@ -35,17 +27,13 @@ class PositionsController < ApplicationController
         @position.position_salaries.set_relevant
         flash[:success] = 'Должность успешно создана.'
         format.html { redirect_to edit_position_path(@position) }
-        #format.json { render :show, status: :created, location: @position }
       else
         flash[:danger] = 'Ошибка создания должности.'
         format.html { render :new }
-        #format.json { render json: @position.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /positions/1
-  # PATCH/PUT /positions/1.json
   def update
     respond_to do |format|
       if @position.update(position_params)
@@ -53,23 +41,19 @@ class PositionsController < ApplicationController
         @position.position_salaries.set_relevant
         flash[:success] = 'Должность успешно отредактирована.'
         format.html { redirect_to edit_position_path(@position) }
-        #format.json { render :show, status: :ok, location: @position }
       else
-        flash[:danger] = 'Ошибка редактирования должности'
+        flash[:danger] = 'Ошибка редактирования должности.'
         format.html { redirect_to edit_position_path(@position) }
-        #format.json { render json: @position.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /positions/1
-  # DELETE /positions/1.json
   def destroy
     @position.update_column(:deleted, true)
     respond_to do |format|
+      PositionSalary.set_irrelevant(@position.id)
       flash[:success] = 'Должность успешно удалена.'
       format.html { redirect_to positions_url }
-      #format.json { head :no_content }
     end
   end
 
